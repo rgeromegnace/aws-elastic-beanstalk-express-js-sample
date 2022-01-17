b40b088a8fe84ca3408cd8b80efff72cd7c53b03
rgeromeg <r.geromegnace@c2i-guadeloupe.com>
2022-01-17 14:18:34 -0400

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Ka zot fè!'));
+app.get('/', (req, res) => res.send('Ka zot fè band de macos!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
